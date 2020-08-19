09519b32ba5a45d3eace326ff455eee7b698cd16
ertugrulozvardar <ertugrul.ozvardar@ozu.edu.tr>
2020-08-19 14:29:25 +0300

    DataBinding features added. Now, we can directly call our widgets like textViews or buttons rather than retrieving from findViewById function

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.aboutme
 
 import android.content.Context
+import android.opengl.Visibility
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
@@ -8,24 +9,31 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    findViewById<Button>(R.id.done_button).setOnClickListener {
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    binding.doneButton.setOnClickListener {
         addNickname(it)
     }
     }
 
     private fun addNickname (view: View){
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        binding.apply {
+            binding.nicknameText.text = binding.nicknameEdit.text
+            invalidateAll()
+            binding.nicknameEdit.visibility = View.GONE
+            binding.doneButton.visibility = View.GONE
+            binding.nicknameText.visibility = View.VISIBLE
+        }
 
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
 
         val inpmng = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         inpmng.hideSoftInputFromWindow(view.windowToken,0)
